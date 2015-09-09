5c77ef8611af1c8d0fb6107809267e0dd7d72577
何永能 <he.yongneng@bestv.com.cn>
2015-09-10 00:48:47 +0800

    动态标题

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = I18n.t('layouts.header.project_name')
+    slogan = I18n.t('layouts.header.slogan')
+    if page_title.empty?
+      "#{base_title} - #{slogan}"
+    else
+      "#{page_title} | #{base_title} - #{slogan}"
+    end
+  end
+
   def active_page(path)
     current_page?(path) ? 'active' : 'none'
   end
